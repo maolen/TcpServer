2d5e5561db421408bbf293e624ddc38549c424c0
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-12-26 20:04:44 +0600

    TCP Server

--- TcpServer/Program.cs
+++ TcpServer/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace TcpServer
 {
@@ -6,7 +9,37 @@ namespace TcpServer
     {
         static void Main(string[] args)
         {
-            
+            var listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 3231);
+            listener.Start();
+            Console.WriteLine("Сервер запущен");
+
+            while(true)
+            {
+                using (var client = listener.AcceptTcpClient())
+                {
+                    Console.WriteLine("Входящее соединение...");
+                    using (var stream = client.GetStream())
+                    {
+                        var resultText = new StringBuilder();
+
+                        while (stream.DataAvailable)
+                        {
+                            var buffer = new byte[1024];
+                            stream.Read(buffer, 0, buffer.Length);
+
+                            resultText.Append(Encoding.UTF8.GetString(buffer));
+                        }
+                        Console.WriteLine($"Данные от клиента: {resultText.ToString()}");
+
+                        var answerData = Encoding.UTF8.GetBytes("Запрос получен...");
+                        stream.Write(answerData, 0, answerData.Length);
+                    }
+                }
+                Console.WriteLine("Соединение закрыто");
+            }
+
+            //listener.Stop();
+            //Console.WriteLine("Сервер остановлен");
         }
     }
 }
